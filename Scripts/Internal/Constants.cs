073690230179174ee4e56911be0bb4042a516b87
Gley <gley.mobi@gmail.com>
2023-09-06 15:27:39 +0300

    Package added

---
+++ Scripts/Internal/Constants.cs
@@ -0,0 +1,8 @@
+namespace Gley.EasyIAP.Internal
+{
+    public class Constants 
+    {
+        public const string DATA_NAME_RUNTIME = "EasyIAPDataRuntime";
+        public const string RESOURCES_FOLDER = "Resources";
+    }
+}
